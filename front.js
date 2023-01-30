d8dcbfa6294830ab38384a9a21b99d5f2aa237ae
Lucas Brito <83874671+lukasbryt@users.noreply.github.com>
2023-01-29 23:09:06 -0300

    Update front.js

--- front.js
+++ front.js
@@ -3,19 +3,19 @@ function hideHome() {
     document.getElementById('start').style.display = 'none';
     document.getElementById('two').style.display = 'block';
     document.getElementById('three').style.display = 'none';
-    document.getElementById('container').style.background = 'url(/img/p2.webp)';
+    document.getElementById('container').style.background = 'url(https://raw.githubusercontent.com/lukasbryt/trilhafront/main/img/p2.webp)';
 }
 
 function home() {
     document.getElementById('start').style.display = 'block';
     document.getElementById('two').style.display = 'none';
     document.getElementById('three').style.display = 'none';
-    document.getElementById('container').style.background = 'url(/img/p1.png)';
+    document.getElementById('container').style.background = 'url(https://raw.githubusercontent.com/lukasbryt/trilhafront/main/img/p1.png)';
 }
 
 function pgthree() {
     document.getElementById('start').style.display = 'none';
     document.getElementById('two').style.display = 'none';
     document.getElementById('three').style.display = 'block';
-    document.getElementById('container').style.background = 'url(/img/p3.webp)';
+    document.getElementById('container').style.background = 'url(https://raw.githubusercontent.com/lukasbryt/trilhafront/main/img/p3.webp)';
 }
