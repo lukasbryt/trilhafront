99f35684f42baa982c42bd9b3f954ca196ea043d
Lucas Brito <83874671+lukasbryt@users.noreply.github.com>
2023-02-03 01:18:29 -0300

    Add files via upload

--- front.js
+++ front.js
@@ -1,21 +1,21 @@
 function hideHome() {
     //esconde tela inicial
-    document.getElementById('start').style.display = 'none';
+    document.getElementById('home').style.display = 'none';
     document.getElementById('two').style.display = 'block';
     document.getElementById('three').style.display = 'none';
-    document.getElementById('container').style.background = 'url(https://raw.githubusercontent.com/lukasbryt/trilhafront/main/img/p2.webp)';
+    document.getElementById('bkimage').style.backgroundImage = 'url(img/p2.webp)';
 }
 
 function home() {
-    document.getElementById('start').style.display = 'block';
+    document.getElementById('home').style.display = 'block';
     document.getElementById('two').style.display = 'none';
     document.getElementById('three').style.display = 'none';
-    document.getElementById('container').style.background = 'url(https://raw.githubusercontent.com/lukasbryt/trilhafront/main/img/p1.png)';
+    document.getElementById('bkimage').style.backgroundImage = 'url(img/p1.webp)';
 }
 
 function pgthree() {
-    document.getElementById('start').style.display = 'none';
+    document.getElementById('home').style.display = 'none';
     document.getElementById('two').style.display = 'none';
     document.getElementById('three').style.display = 'block';
-    document.getElementById('container').style.background = 'url(https://raw.githubusercontent.com/lukasbryt/trilhafront/main/img/p3.webp)';
+    document.getElementById('bkimage').style.backgroundImage = 'url(img/p3.webp)';
 }
